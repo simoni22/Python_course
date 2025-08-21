007b681df190b81e048536c56279a67de0683a47
שון סימוני <seansi@auth.ad.bgu.ac.il>
2025-08-21 11:56:07 +0300

    html's added for products window in shop

--- Pyshop_Django/products/views.py
+++ Pyshop_Django/products/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product, Offer
 
 
 # /products -> index
 def index(request):
-    return HttpResponse('Hello World')
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
 
 
 def index_new(request):
