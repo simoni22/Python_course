2562d4b2d7d0e8d8e7517642ca8ed45e8dea3aa3
שון סימוני <seansi@auth.ad.bgu.ac.il>
2025-08-21 10:35:56 +0300

    Activate server + products app

--- Pyshop_Django/products/views.py
+++ Pyshop_Django/products/views.py
@@ -1,2 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
+# /products -> index
+def index(request):
+    return HttpResponse('Hello World')
+
+
+def index_new(request):
+    return HttpResponse('New Products')
